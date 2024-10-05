e0b62b22e010bcc19427cee1a6c30fbbe26e4c95
Luís Fernando <luis.fernando@alunos.iuul.com.br>
2024-10-05 13:05:48 -0300

    add: class enums

---
+++ src/main/java/com/aluguelbicicleta/aluguelbicicleta/model/enums/StatusBicicleta.java
@@ -0,0 +1,5 @@
+package com.aluguelbicicleta.aluguelbicicleta.model.enums;
+
+public enum StatusBicicleta {
+    DISPONIVEL, EM_USO, NOVA, APOSENTADA, REPARO_SOLICITADO, EM_REPARO
+}
---
+++ src/main/java/com/aluguelbicicleta/aluguelbicicleta/model/enums/StatusTranca.java
@@ -0,0 +1,5 @@
+package com.aluguelbicicleta.aluguelbicicleta.model.enums;
+
+public enum StatusTranca {
+    LIVRE, OCUPADA, NOVA, APOSENTADA, EM_REPARO
+}
