244b5a60ad07c673d3efb796e19090f27b3f6d28
Luís Fernando <luis.fernando@alunos.iuul.com.br>
2024-10-05 09:16:06 -0300

    first commit

---
+++ src/main/java/com/aluguelbicicleta/aluguelbicicleta/AluguelbiciletaApplication.java
@@ -0,0 +1,13 @@
+package com.aluguelbicicleta.aluguelbicicleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelbiciletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelbiciletaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluguelbicicleta/aluguelbicicleta/AluguelbiciletaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluguelbicicleta.aluguelbicicleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelbiciletaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
