349b461e5d9a8c97fec7a35ee4bdd2e21d2420dd
Luís Fernando <luis.fernando@alunos.iuul.com.br>
2024-10-11 14:19:58 -0300

    add: controller

---
+++ src/main/java/com/aluguelbicicleta/aluguelbicicleta/controllers/BicicletaController.java
@@ -0,0 +1,44 @@
+package com.aluguelbicicleta.aluguelbicicleta.controllers;
+
+import java.util.UUID;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aluguelbicicleta.aluguelbicicleta.model.Bicicleta;
+import com.aluguelbicicleta.aluguelbicicleta.repository.BicicletaRepository;
+
+import jakarta.transaction.Transactional;
+
+@RestController
+@RequestMapping("/bicicleta")
+public class BicicletaController {
+    
+    @Autowired
+    private BicicletaRepository bicicletaRepository;
+
+    @GetMapping
+    public List<Bicicleta> findAll(){
+        return bicicletaRepository.findAll();
+    }
+
+    @Transactional
+    @GetMapping("/{id}")
+    public Optional<Bicicleta> findById(@PathVariable UUID id){
+        return bicicletaRepository.findById(id);
+    }
+
+    @Transactional
+    @PostMapping
+    public Bicicleta addBicicleta(@RequestBody Bicicleta b){
+        return bicicletaRepository.save(b);
+    }
+}
