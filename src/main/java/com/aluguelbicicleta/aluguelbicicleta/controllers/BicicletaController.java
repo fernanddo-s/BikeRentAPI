a146c2a7304e1dec926651bcff0f76140c59bf98
Luís Fernando <luis.fernando@alunos.iuul.com.br>
2024-10-14 11:14:05 -0300

    refactor: nome do metodo e retirando importa desnecessario

--- src/main/java/com/aluguelbicicleta/aluguelbicicleta/controllers/BicicletaController.java
+++ src/main/java/com/aluguelbicicleta/aluguelbicicleta/controllers/BicicletaController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,7 +37,7 @@ public class BicicletaController {
 
     @Transactional
     @PostMapping
-    public Bicicleta addBicicleta(@RequestBody Bicicleta b){
+    public Bicicleta create(@RequestBody Bicicleta b){
         return bicicletaRepository.save(b);
     }
 }
