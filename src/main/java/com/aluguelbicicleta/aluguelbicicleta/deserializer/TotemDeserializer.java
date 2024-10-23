dcc84d4a52c92e675dca0f0dd89e8b4c84881555
Luís Fernando <luis.fernando@alunos.iuul.com.br>
2024-10-23 20:45:42 -0300

    fix: classe de deserializar totem

--- src/main/java/com/aluguelbicicleta/aluguelbicicleta/deserializer/TotemDeserializer.java
+++ src/main/java/com/aluguelbicicleta/aluguelbicicleta/deserializer/TotemDeserializer.java
@@ -2,20 +2,41 @@ package com.aluguelbicicleta.aluguelbicicleta.deserializer;
 
 import com.aluguelbicicleta.aluguelbicicleta.model.Totem;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+
 import java.io.IOException;
-import java.util.UUID;
 
 public class TotemDeserializer extends JsonDeserializer<Totem> {
 
     @Override
-    public Totem deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
-            throws IOException, JsonProcessingException {
-        String uuid = jsonParser.getText();
+    public Totem deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        JsonNode node = p.getCodec().readTree(p);
+
         Totem totem = new Totem();
-        totem.setId(UUID.fromString(uuid));
+
+        if (node.isNumber()) {
+            totem.setId(node.asLong());
+        }
+        else if (node.isObject()) {
+            if (node.has("id")) {
+                totem.setId(node.get("id").asLong());
+            }
+            if (node.has("localizacao")) {
+                totem.setLocalizacao(node.get("localizacao").asText());
+            }
+            if (node.has("capacidade")) {
+                totem.setCapacidade(node.get("capacidade").asInt());
+            }
+            if (node.has("descricao")) {
+                totem.setDescricao(node.get("descricao").asText());
+            }
+        }
+        else {
+            throw new IOException("Formato de entrada inválido para Totem.");
+        }
+
         return totem;
     }
 }
