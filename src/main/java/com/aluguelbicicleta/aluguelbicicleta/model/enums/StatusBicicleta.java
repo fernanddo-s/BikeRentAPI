5178f41e5c531d343ab05348f2f45e330288a7fa
Luís Fernando <luis.fernando@alunos.iuul.com.br>
2024-10-23 18:47:06 -0300

    fix: funcoes para conversão dos enums

--- src/main/java/com/aluguelbicicleta/aluguelbicicleta/model/enums/StatusBicicleta.java
+++ src/main/java/com/aluguelbicicleta/aluguelbicicleta/model/enums/StatusBicicleta.java
@@ -1,5 +1,18 @@
 package com.aluguelbicicleta.aluguelbicicleta.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum StatusBicicleta {
-    DISPONIVEL, EM_USO, NOVA, APOSENTADA, REPARO_SOLICITADO, EM_REPARO
+    DISPONIVEL, EM_USO, NOVA, APOSENTADA, REPARO_SOLICITADO, EM_REPARO;
+
+    @JsonCreator
+    public static StatusBicicleta fromString(String value) {
+        return StatusBicicleta.valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name().toUpperCase();
+    }
 }
--- src/main/java/com/aluguelbicicleta/aluguelbicicleta/model/enums/StatusTranca.java
+++ src/main/java/com/aluguelbicicleta/aluguelbicicleta/model/enums/StatusTranca.java
@@ -1,5 +1,18 @@
 package com.aluguelbicicleta.aluguelbicicleta.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum StatusTranca {
-    LIVRE, OCUPADA, NOVA, APOSENTADA, EM_REPARO
+    LIVRE, OCUPADA, NOVA, APOSENTADA, EM_REPARO;
+
+    @JsonCreator
+    public static StatusTranca fromString(String value) {
+        return StatusTranca.valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name().toUpperCase();
+    }
 }
