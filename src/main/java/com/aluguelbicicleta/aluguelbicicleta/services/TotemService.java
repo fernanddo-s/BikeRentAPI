ac61b6de8f1f6184fa177065b8fe33c3c65ef59d
Luís Fernando <luis.fernando@alunos.iuul.com.br>
2024-10-23 20:46:17 -0300

    add: camada de service

--- src/main/java/com/aluguelbicicleta/aluguelbicicleta/services/TotemService.java
+++ src/main/java/com/aluguelbicicleta/aluguelbicicleta/services/TotemService.java
@@ -1,5 +1,8 @@
 package com.aluguelbicicleta.aluguelbicicleta.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +15,40 @@ public class TotemService {
     @Autowired
     private TotemRepository totemRepository;
 
-    public Totem update(Totem t){
-        var totem = totemRepository.findById(t.getId());
-        if(totem.isPresent()){
-            return totemRepository.save(t);
+    public Totem create(Totem t){
+        return totemRepository.save(t);
+    }
+
+    public Optional<Totem> findById(Long id){
+        return totemRepository.findById(id);
+    }
+
+    public List<Totem> findAll(){
+        return totemRepository.findAll();
+    }
+
+    public Totem update(Long id, Totem t){
+        Optional<Totem> optionalTotem = totemRepository.findById(id);
+
+        if (optionalTotem.isPresent()) {
+            Totem totemExistente = optionalTotem.get();
+            if(t.getCapacidade() != null){
+                totemExistente.setCapacidade(t.getCapacidade());
+            }
+            if(t.getDescricao() != null){
+                totemExistente.setDescricao(t.getDescricao());
+            }
+            if(t.getLocalizacao() != null){
+                totemExistente.setLocalizacao(t.getLocalizacao());
+            }
+            
+            return totemRepository.save(totemExistente);
+        } else {
+            throw new RuntimeException("Bicicleta com ID " + id + " não encontrada");
         }
-        return null;
+    }
+
+    public void deleteById(Long id){
+        totemRepository.deleteById(id);
     }
 }
