1f7facf199dc711f5d2748ba44dd834f542347e1
Luís Fernando <luis.fernando@alunos.iuul.com.br>
2024-10-24 12:36:48 -0300

    add: query de listar bicicletas de um totem

--- src/main/java/com/aluguelbicicleta/aluguelbicicleta/repository/BicicletaRepository.java
+++ src/main/java/com/aluguelbicicleta/aluguelbicicleta/repository/BicicletaRepository.java
@@ -1,9 +1,16 @@
 package com.aluguelbicicleta.aluguelbicicleta.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.aluguelbicicleta.aluguelbicicleta.model.Bicicleta;
 
 public interface BicicletaRepository extends JpaRepository<Bicicleta, Long>{
     
+    @Query("select b from Bicicleta b \n" + 
+            "inner join Tranca t on t.bicicleta.id = b.id\n" + 
+            "where t.totem.id = :totemId")
+    List<Bicicleta> findBicicletasByTotemId(@Param("totemId")Long totemId);
 }
