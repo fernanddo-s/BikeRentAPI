038279db5926e812da884226a02c2450ea75e7fe
Luís Fernando <luis.fernando@alunos.iuul.com.br>
2024-11-08 22:27:21 -0300

    página padrão

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,19 @@
 <script setup>
 import Header from '@/components/header/Header.vue';
+import Totem from '@/components/totem/Totem.vue';
 </script>
 
 <template>
-  <Header/>
+  <Header></Header>
+  <div class="table">
+    <Totem/>
+  </div>
 </template>
+
+<style scoped>
+.table{
+  width: 100%;
+  justify-content: center;
+  display: flex;
+}
+</style>
