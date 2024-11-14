7e2b882f67915cd7392ca29606b0898fd31dd1bc
Luís Fernando <luis.fernando@alunos.iuul.com.br>
2024-11-14 16:19:48 -0300

    add: componente para vizualização de usuarios

---
+++ frontend/src/views/UsersView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Header from '@/components/header/Header.vue';
+import { reactive } from 'vue';
+
+const variaveis = reactive({
+  users: [
+    {
+      "cpf": "00900900909",
+      "login": "admin@mail.com",
+      "idade": 23,
+      "role": "ADMIN",
+      "matricula": "479528",
+      "nome": "ADMIN do Sistema",
+      "senha": "admin123"
+    }
+  ],
+  headers: [
+    { title: 'CPF', key: 'cpf'},
+    { title: 'Matricula', key: 'matricula' },
+    { title: 'Nome', key: 'nome' },
+    { title: 'Tipo de Acesso', key: 'role' }
+  ]
+})
+
+</script>
+
+<template>
+<Header></Header>
+<v-data-table :items="variaveis.users" :headers="variaveis.headers">
+
+</v-data-table>
+</template>
