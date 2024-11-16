f9d60753789a1c5e8a1d14db62431ae9ae85d8f3
Luís Fernando <luis.nando.acarau@gmail.com>
2024-11-16 19:47:07 -0300

    fix: corrigindo tela de controle de bicicletas

--- frontend/src/views/BicicletaView.vue
+++ frontend/src/views/BicicletaView.vue
@@ -1,15 +1,9 @@
 <script setup>
 import { onMounted, reactive } from 'vue';
-import { getAllBicicletasByTotem } from '@/services/TotemService';
-
-const props = defineProps({
-  localizacaoTotem: String,
-  idTotem: Number
-})
-
-async function getBicicletas(idTotem) {
-  return getAllBicicletasByTotem(idTotem)
-}
+import { getBicicletas } from '@/services/BicicletaService';
+import Header from '@/components/header/Header.vue';
+import DialogDelete from '@/components/dialogDelete/DialogDelete.vue';
+import BicicletaForm from '@/components/forms/BicicletaForm.vue';
 
 const variaveis = reactive({
   bicicletas: [], 
@@ -24,24 +18,46 @@ const variaveis = reactive({
 })
 
 onMounted(async () => {
-  variaveis.bicicletas = await getBicicletas(props.idTotem);
+  variaveis.bicicletas = await getBicicletas();
 });
 
 </script>
 
 <template>
-  <v-data-table :items="variaveis.bicicletas" :headers="variaveis.headers" :hide-default-footer="true">
-    <!-- <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>Bicicletas do totem {{ props.localizacaoTotem }}</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-        <v-btn icon="mdi-plus"></v-btn>
-      </v-toolbar>
-    </template> -->
-    <template v-slot:item.actions="{ item }">
-      <v-btn>Alugar</v-btn>
-      {{ props.idTotem }}
-    </template>
-  </v-data-table>
+  <Header></Header>
+  <div class="div">
+    <v-data-table :items="variaveis.bicicletas" :headers="variaveis.headers" class="data-table">
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Bicicletas</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <BicicletaForm :icon="'mdi-plus'"></BicicletaForm>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon class="btn-actions" size="x-small">
+          <BicicletaForm :bicicleta="item" :icon="'mdi-pencil'" :editando="true"></BicicletaForm>
+        </v-icon>
+        <v-icon size="x-small">
+          <DialogDelete :item-id="item.id" :delete-func="apagarBicicleta"></DialogDelete>
+        </v-icon>
+      </template>
+    </v-data-table>
+  </div>
+  
 </template>
+
+<style scoped>
+.div{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.data-table{
+  padding: 0;
+  margin: 0;
+  border: none;
+  width: 90%;
+}
+</style>
