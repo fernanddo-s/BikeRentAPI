640eb9aca9c6666798d91903ee041be93354ddb8
Luís Fernando <luis.nando.acarau@gmail.com>
2024-11-16 19:45:55 -0300

    refactor: nome do arquivo de service

---
+++ frontend/src/services/BicicletaService.js
@@ -0,0 +1,47 @@
+/**
+ * Configurar chamada para api em java
+ * configurar token(deixar copiando por enquanto)
+ */
+import axios from 'axios'
+
+const API_URL = 'http://localhost:8080'
+
+export async function getBicicletas() {
+  try {
+    const response = await axios.get(`${API_URL}/bicicleta`)
+    return Array.isArray(response.data) ? response.data : []
+  } catch (error) {
+    console.error('Erro ao buscar bicicletas:', error)
+    return []
+  }
+}
+
+export async function create(bicicleta) {
+  try {
+    const response = await axios.post(`${API_URL}/bicicleta`, bicicleta)
+    return Array.isArray(response.data) ? response.data : []
+  } catch (error) {
+    console.error('Erro ao buscar bicicletas:', error)
+    return []
+  }
+}
+
+export async function update(id, bicicleta) {
+  try {
+    const response = await axios.put(`${API_URL}/bicicleta/${id}`, bicicleta)
+    return Array.isArray(response.data) ? response.data : []
+  } catch (error) {
+    console.error('Erro ao buscar bicicletas:', error)
+    return []
+  }
+}
+
+export async function apagar(id) {
+  try {
+    const response = await axios.delete(`${API_URL}/bicicleta/${id}`)
+    return Array.isArray(response.data) ? response.data : []
+  } catch (error) {
+    console.error('Erro ao buscar bicicletas:', error)
+    return []
+  }
+}
