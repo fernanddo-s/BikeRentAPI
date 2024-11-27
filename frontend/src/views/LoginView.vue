bbfda4167f67d7f3760d6af4ab8e9c40acf5e617
Luís Fernando <luis.nando.acarau@gmail.com>
2024-11-26 21:58:14 -0300

    add: arquivos das telas de login e singin do sistema

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <section class="classe1">
+    <div class="classe2">
+      <v-card title="Login">
+        <v-card-text >
+          <v-text-field type="text" label="Login" clearable=""></v-text-field>
+          <v-text-field type="password" label="Senha" clearable=""></v-text-field>
+          <a href="">Não tenho cadastro</a>
+          <v-btn color="primary">ENTRAR</v-btn>
+        </v-card-text>
+      </v-card>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.classe1{
+  display: flex;
+  justify-content: center;
+}
+
+.classe2{
+  width: 500px;
+  height: 300px;
+  background-color: rebeccapurple;
+}
+</style>
