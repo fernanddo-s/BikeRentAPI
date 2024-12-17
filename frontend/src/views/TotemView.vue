d6758629dd691306642798deef19714b1785a6f3
Luís Fernando <luis.nando.acarau@gmail.com>
2024-12-17 09:28:03 -0300

    fix: totemview

---
+++ frontend/src/views/TotemView.vue
@@ -0,0 +1,93 @@
+<script setup>
+import Header from '@/components/header/Header.vue';
+import { onMounted, reactive } from 'vue';
+import TotemForm from '@/components/forms/TotemForm.vue';
+import DialogDelete from '@/components/dialogDelete/DialogDelete.vue';
+import Bicicleta from '@/components/bicicleta/Bicicleta.vue';
+import { getTotems } from '@/services/TotemService';
+
+const variaveis = reactive({
+  totems: [],
+  headers: [
+    { title: "Localização", key: "localizacao" },
+    { title: "Capacidade", key: "capacidade" },
+    { title: "Descrição", key: "descricao", sortable: false },
+    { title: "Ações", key: "actions", sortable: false, align: 'center' },
+  ],
+  totem:{
+    localizacao: '',
+    capacidade: '',
+    descricao: '',
+  },
+  dialogDelete:false
+})
+
+function loadItems(){
+  let response = getTotems();
+  variaveis.totems = response;
+}
+
+function novoTotem(totem){
+  variaveis.totems.push({localizacao: totem.localizacao, capacidade: totem.capacidade, descricao: totem.descricao})
+}
+
+onMounted( async () => {
+  loadItems();
+})
+</script>
+
+<template>
+  <Header></Header>
+  <div class="div">
+    <v-data-table :items="variaveis.totems" :headers="variaveis.headers" class="data-table" show-expand="">
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Totens</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <totem-form @atualizar-tabela="novoTotem" :totem="variaveis.totem" :icon="'mdi-plus'"></totem-form>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon class="mr-3 btn-actions">
+          mdi-map-marker
+        </v-icon>
+        <v-icon class="btn-actions">
+          mdi-bicycle
+        </v-icon>
+        <v-icon class="btn-actions" size="x-small">
+          <TotemForm :totem="item" :icon="'mdi-pencil'" :editando="true"></TotemForm>
+        </v-icon>
+        <v-icon size="x-small">
+          <DialogDelete :item-id="item.id" ></DialogDelete>
+        </v-icon>
+      </template>
+      <template v-slot:expanded-row="{ columns, item }">
+        <tr>
+          <td :colspan="columns.length">
+            <Bicicleta :localizacao-totem="item.localizacao" :id-totem="item.id"></Bicicleta>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<style scoped>
+.div{
+  width: 100%;
+  justify-content: center;
+  display: flex;
+}
+
+.data-table{
+  padding: 0;
+  margin: 0;
+  border: none;
+  width: 90%;
+}
+
+.btn-actions{
+  margin-right: 5px;
+}
+</style>
