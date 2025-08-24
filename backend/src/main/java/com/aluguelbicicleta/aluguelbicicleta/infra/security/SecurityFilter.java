faa38490b0a06b12d64598aa0536c06b02d3869c
Luís Fernando <luis.nando.acarau@gmail.com>
2025-08-24 18:04:33 -0300

    config(SecurityFilter): configurando filtro de seguraça com spring security

--- backend/src/main/java/com/aluguelbicicleta/aluguelbicicleta/infra/security/SecurityFilter.java
+++ backend/src/main/java/com/aluguelbicicleta/aluguelbicicleta/infra/security/SecurityFilter.java
@@ -30,9 +30,6 @@ public class SecurityFilter extends OncePerRequestFilter{
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         if (token != null) {
-            /*
-             * Erro está aqui!!!
-             */
             var login = tokenService.validarToken(token);
             UserDetails user = userRepository.findByLogin(login);
 
